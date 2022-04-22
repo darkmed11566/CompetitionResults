62641e3ab0f83719407bba69463b301c6ab8b54d
Николай Толкачев <darkmed@mail.ru>
2022-04-22 21:46:55 +0300

    Добавьте файлы проекта.

---
+++ EnumsAndConstants/ListOfCountries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionResults.EnumsAndConstants
+{
+    public enum ListOfCountries
+    {
+      BLR=1,
+      RUS=2,
+      LTV=3
+    }
+}
---
+++ Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionResults.Models
+{
+    public class BaseModel
+    {
+        public long Id { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
