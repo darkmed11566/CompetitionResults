3e3d75a9aaa523163ee75ab40fdb3e3ddf730ab9
Николай Толкачев <darkmed@mail.ru>
2022-05-20 00:45:09 +0300

    AddSportsman

---
+++ EnumsAndConstants/Rangs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionResults.EnumsAndConstants
+{
+    public enum Rangs
+    {
+        NR = 1,
+        III = 2,
+        II = 3,
+        I = 4,
+        CMS = 5,
+        MS = 6
+    }
+}
---
+++ Models/Sportsman.cs
@@ -0,0 +1,20 @@
+﻿using CompetitionResults.EnumsAndConstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionResults.Models
+{
+    public class Sportsman:BaseModel
+    {
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Sex Sex { get; set; }
+        public ListOfCountries Country { get; set; }
+        public Rangs Rang { get; set; }
+        public string NameOfClub { get; set; }
+        public string NamesOfCoaches { get; set; }
+    }
+}
