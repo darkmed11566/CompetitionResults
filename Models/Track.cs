fd39d4407e38484c8dd5fb737858fa4b9c97e40a
Николай Толкачев <darkmed@mail.ru>
2022-05-24 23:50:24 +0300

    AddTrackAndAddDeletedButton

---
+++ Models/Track.cs
@@ -0,0 +1,14 @@
+﻿using CompetitionResults.EnumsAndConstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionResults.Models
+{
+    public class Track : BaseModel
+    {
+        public TrackType  TrackType { get; set; }
+        //public Competition CompetitionName { get; set; }
+    }
+}
