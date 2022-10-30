20be2e9b51286772d86aa7121ce8384234c81be2
Николай Толкачев <darkmed@mail.ru>
2022-10-30 15:10:19 +0300

    Master

---
+++ Models/Competitioner.cs
@@ -0,0 +1,15 @@
+﻿using CompetitionResults.EnumsAndConstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompetitionResults.Models
+{
+    public class Competitioner:BaseModel
+    {
+        public int Number { get; set; }
+        public BoatClasses Class { get; set; }
+       
+    }
+}
