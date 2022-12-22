364d1804182c135d1d7cd404b6e728c18e37c91b
Николай Толкачев <darkmed@mail.ru>
2022-12-22 21:05:59 +0300

    AddLinkSC

---
+++ Migrations/20221222172415_AddLinkSC.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CompetitionResults.Migrations
+{
+    public partial class AddLinkSC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "SportsmanId",
+                table: "Competitioners",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Competitioners_SportsmanId",
+                table: "Competitioners",
+                column: "SportsmanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Competitioners_Sportsmens_SportsmanId",
+                table: "Competitioners",
+                column: "SportsmanId",
+                principalTable: "Sportsmens",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Competitioners_Sportsmens_SportsmanId",
+                table: "Competitioners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Competitioners_SportsmanId",
+                table: "Competitioners");
+
+            migrationBuilder.DropColumn(
+                name: "SportsmanId",
+                table: "Competitioners");
+        }
+    }
+}
--- Pages/AddCompetitioners.razor.cs
+++ Pages/AddCompetitioners.razor.cs
@@ -1,5 +1,9 @@
-﻿using CompetitionResults.EnumsAndConstants;
+﻿using CompetitionResults.Data;
+using CompetitionResults.EnumsAndConstants;
 using CompetitionResults.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +13,27 @@ namespace CompetitionResults.Pages
 {
     public partial class AddCompetitioners
     {
+        [Inject]
+        protected IServiceScopeFactory serviceScopeFactory { get; set; }
+
         private IEnumerable<Competitioner> competitioner = new List<Competitioner>();
 
-        protected override void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
-            competitioner = competitionerRepository.GetAll();
+            using var scope = serviceScopeFactory.CreateScope();
 
+            var context = scope.ServiceProvider.GetRequiredService<WebContext>();
+            competitioner = await context.Competitioners.AsNoTracking().ToListAsync();
+            SportsmansForSelect = await context.Sportsmens.AsNoTracking()
+                .Where(x=> x.IsActive == true).ToListAsync();
+            newSportsman = SportsmansForSelect.OrderBy(x => x.Id).FirstOrDefault()?.Id ?? 0;
         }
 
         private int newNumber;
         private BoatClasses newClass;
-
-        private void AddCompetitioner()
+        private IEnumerable<Sportsman> SportsmansForSelect = new List<Sportsman>();
+        private long newSportsman;
+        private async Task AddCompetitioner()
         {
 
             var dbCompetitioner = new Competitioner();
@@ -29,9 +42,18 @@ namespace CompetitionResults.Pages
             dbCompetitioner.BoatClass = newClass;
             dbCompetitioner.IsActive = true;
             dbCompetitioner.StatusInTrack = StatusSportsmanInTrack.RegisteredForCompetition;
+            dbCompetitioner.SportsmanId = newSportsman;
 
-            competitionerRepository.Save(dbCompetitioner);
+            using var scope = serviceScopeFactory.CreateScope();
 
+            var context = scope.ServiceProvider.GetRequiredService<WebContext>();
+            await context.Competitioners.AddAsync(dbCompetitioner);
+            await context.SaveChangesAsync();
+
+        }
+        protected void TrackSelected(ChangeEventArgs args)
+        {
+            var x = args.Value;
         }
 
         private void DeleteCompetitioner(long id)
